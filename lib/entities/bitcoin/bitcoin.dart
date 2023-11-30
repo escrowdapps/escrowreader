463c2e43c544142b2ef756dedf5440db3f304b41
Evgeny <e.litvinenko@mute-lab.com>
2023-11-30 13:55:47 +0400

    implemented getReplacedUri

--- lib/entities/bitcoin/bitcoin.dart
+++ lib/entities/bitcoin/bitcoin.dart
@@ -3,7 +3,33 @@ import 'package:hive/hive.dart';
 class Bitcoin {
   static const String HIVE_SETTINGS_KEY = 'settings';
 
+  static Uri BLOCKSTREAM_URL = Uri(
+      scheme: 'https',
+      host: 'blockstream.info',
+      path: 'api/address/wallet/utxo');
+
   static Future<void> getUTXO() async {
-    var box = await Hive.openBox(HIVE_SETTINGS_KEY);
+    // var box = await Hive.openBox(HIVE_SETTINGS_KEY);
+  }
+
+  static Uri? getReplacedUri({String walletId = '', Uri? customUrl}) {
+    if (walletId.isEmpty) {
+      throw const FormatException('wallet id not passed');
+    }
+
+    if (customUrl != null) {
+      Uri url = customUrl.replace(path: 'api/address/$walletId/utxo');
+
+      return url;
+    }
+
+    if (walletId.isNotEmpty) {
+      Uri url =
+          Bitcoin.BLOCKSTREAM_URL.replace(path: 'api/address/$walletId/utxo');
+
+      return url;
+    }
+
+    return null;
   }
 }
---
+++ lib/entities/bitcoin/bitcoin.unit.dart
@@ -0,0 +1,14 @@
+import 'package:test/test.dart';
+import 'package:app/entities/bitcoin/bitcoin.dart';
+
+void main() {
+  test('getReplacedUri', () {
+    expect(Bitcoin.getReplacedUri(walletId: 'test').toString(),
+        equals('https://blockstream.info/api/address/test/utxo'));
+
+    Uri testUri = Uri(scheme: 'https', host: 'google.com', path: 'wallet/test');
+
+    expect(Bitcoin.getReplacedUri(customUrl: testUri, walletId: 'test-wallet').toString(),
+        equals('https://google.com/api/address/test-wallet/utxo'));
+  });
+}
